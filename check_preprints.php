3cb2fed26f24d363fc3c11947ef4bd77df157d24
fabiodfn <fabiodefantis@libero.it>
2015-04-02 10:46:15 +0200

    versione test

--- check_preprints.php
+++ check_preprints.php
@@ -39,121 +39,131 @@
         </script>
     </head>
     <body>
-        <div id="header-wrapper">
-            <div class="container">
-                <div class="row">
-                    <div class="12u">
-                        <header id="header">
-                            <h1><a href="#" id="logo">DMI Preprints</a></h1>
-                            <nav id="nav">
-                                <a href="main.php">preprint search</a>
-                                <a href="reserved.php" class="current-page-item">Reserved Area</a>
-                            </nav>
-                        </header>
+        <?php
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . 'authorization/sec_sess.php';
+        sec_session_start();
+        if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] < 86400)) {
+            if ($_SESSION['logged_type'] === "mod") {
+                //sessione moderatore
+                ?>
 
+
+                <div id="header-wrapper">
+                    <div class="container">
+                        <div class="row">
+                            <div class="12u">
+                                <header id="header">
+                                    <h1><a href="#" id="logo">DMI Preprints</a></h1>
+                                    <nav id="nav">
+                                        <a href="main.php">preprint search</a>
+                                        <a href="reserved.php" class="current-page-item">Reserved Area</a>
+                                    </nav>
+                                </header>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div id="div_menu_ricerca" class="contenitore"><center>PREPRINTS CHECK</center><br/><br/>
-            <center><table>
-                    <tr align="right"><td>Go to arXiv panel:&nbsp&nbsp&nbsp</td>
-                    <form name="f1" action="arXiv_panel.php" method="POST">
-                        <td><input type="submit" name="bottoni4" value="Back" id="bottone_keyword" class="bottoni"></td>
-                    </form></tr>
-                    <tr align="right"><td>Insert all current preprints into database:&nbsp&nbsp&nbsp<br/></td>
-                    <form name="f2" action="check_preprints.php" method="POST">
-                        <td><input type="submit" name="bottoni5" value="Insert" id="bottone_keyword" class="bottoni"></td>
-                    </form></tr>
-                </table></center>
-        </div>
-        <div>
-            <?php
-            include_once($_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . 'arXiv/insert_remove_db.php');
-            include_once($_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . 'arXiv/arXiv_parsing.php');
-            #leggere cartella...
-            #base link
-            $base = "./arXiv/pdf_downloads/";
-            #Imposto la directory da leggere
-            $directory = $_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . "arXiv/pdf_downloads/";
-            #Apriamo una directory e leggiamone il contenuto.
-            if (is_dir($directory)) {
-                #Apro l'oggetto directory
-                if ($directory_handle = opendir($directory)) {
-                    echo "<form name='f3' action='check_preprints.php' id='f1' method='POST'><center><table style='text-align:center;'>";
-                    #Scorro l'oggetto fino a quando non è termnato cioè false
-                    echo "<tr style='height: 30px; width:200px'><td colspan='2' style='width:250px'>PDF LIST:</td></tr><tr style='height: 30px'><td>Name:</td><td>Select all/<br/>Select<br/></td></tr><tr style='height: 30px; width:200px'><td></td><td><input type='checkbox' name='checkall' onclick='checkedAll(f1);'></td></tr>";
-                    $i = 0;
-                    $y = 1;
-                    while (($file = readdir($directory_handle)) !== false) {
-                        #Se l'elemento trovato è diverso da una directory
-                        #o dagli elementi . e .. lo visualizzo a schermo
-                        if ((!is_dir($file)) & ($file != ".") & ($file != "..")) {
-                            $array[$i] = $file;
-                            echo "<tr style='height: 30px'><td>$y.&nbsp&nbsp&nbsp<a href=" . $base . $file . " onclick='window.open(this.href);return false' title='" . $file . "'>" . $file . "</a></td><td><input type='checkbox' name='" . $i . "' value='checked'/></td></tr>";
-                            $i++;
-                            $y++;
+                <div id="div_menu_ricerca" class="contenitore"><center>PREPRINTS CHECK</center><br/><br/>
+                    <center><table>
+                            <tr align="right"><td>Go to arXiv panel:&nbsp&nbsp&nbsp</td>
+                            <form name="f1" action="arXiv_panel.php" method="POST">
+                                <td><input type="submit" name="bottoni4" value="Back" id="bottone_keyword" class="bottoni"></td>
+                            </form></tr>
+                            <tr align="right"><td>Insert all current preprints into database:&nbsp&nbsp&nbsp<br/></td>
+                            <form name="f2" action="check_preprints.php" method="POST">
+                                <td><input type="submit" name="bottoni5" value="Insert" id="bottone_keyword" class="bottoni"></td>
+                            </form></tr>
+                        </table></center>
+                </div>
+                <div>
+                    <?php
+                    include_once($_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . 'arXiv/insert_remove_db.php');
+                    include_once($_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . 'arXiv/arXiv_parsing.php');
+                    #leggere cartella...
+                    #base link
+                    $base = "./arXiv/pdf_downloads/";
+                    #Imposto la directory da leggere
+                    $directory = $_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . "arXiv/pdf_downloads/";
+                    #Apriamo una directory e leggiamone il contenuto.
+                    if (is_dir($directory)) {
+                        #Apro l'oggetto directory
+                        if ($directory_handle = opendir($directory)) {
+                            echo "<form name='f3' action='check_preprints.php' id='f1' method='POST'><center><table style='text-align:center;'>";
+                            #Scorro l'oggetto fino a quando non è termnato cioè false
+                            echo "<tr style='height: 30px; width:200px'><td colspan='2' style='width:250px'>PDF LIST:</td></tr><tr style='height: 30px'><td>Name:</td><td>Select all/<br/>Select<br/></td></tr><tr style='height: 30px; width:200px'><td></td><td><input type='checkbox' name='checkall' onclick='checkedAll(f1);'></td></tr>";
+                            $i = 0;
+                            $y = 1;
+                            while (($file = readdir($directory_handle)) !== false) {
+                                #Se l'elemento trovato è diverso da una directory
+                                #o dagli elementi . e .. lo visualizzo a schermo
+                                if ((!is_dir($file)) & ($file != ".") & ($file != "..")) {
+                                    $array[$i] = $file;
+                                    echo "<tr style='height: 30px'><td>$y.&nbsp&nbsp&nbsp<a href=" . $base . $file . " onclick='window.open(this.href);return false' title='" . $file . "'>" . $file . "</a></td><td><input type='checkbox' name='" . $i . "' value='checked'/></td></tr>";
+                                    $i++;
+                                    $y++;
+                                }
+                            }
+                            echo "</table><br/><input type='submit' name='bottoni6' value='Delete' id='bottone_keyword' class='bottoni'></center></form><br/>";
+                            #Chiudo la lettura della directory.
+                            closedir($directory_handle);
                         }
                     }
-                    echo "</table><br/><input type='submit' name='bottoni6' value='Delete' id='bottone_keyword' class='bottoni'></center></form><br/>";
-                    #Chiudo la lettura della directory.
-                    closedir($directory_handle);
-                }
-            }
-            $z = 0;
-            $lunghezza = $i;
-            if (isset($_POST['bottoni5'])) {
-                if ($i == 0) {
-                    echo "<center>NO PREPRINTS!</center>";
-                } else {
-                    #richiamo funzione per inserire i pdf all'interno del database
-                    insert_pdf();
-                    echo "<center>HAVE BEEN INCLUEDED " . $i . " PREPRINTS INTO DATABASE!</center>";
-                    #aggiorno la pagina dopo 2 secondi
-                    page_redirect("./check_preprints.php");
-                }
-            }
-            #eliminazione pdf...
-            if (isset($_POST['bottoni6'])) {
-                for ($j = 0; $j < $lunghezza; $j++) {
-                    $percorso = $base . $array[$j];
-                    $delete = $_POST[$j];
-                    if ($delete == "checked") {
-                        $z++;
-                        if (is_dir($directory)) {
-                            if ($directory_handle = opendir($directory)) {
-                                while (($file = readdir($directory_handle)) !== false) {
-                                    if ((!is_dir($file)) & ($file != ".") & ($file != "..")) {
-                                        if ($file == $array[$j]) {
-                                            #cancello file...
-                                            unlink($percorso);
-                                            #cancello riga database...
-                                            remove_preprints($array[$j]);
+                    $z = 0;
+                    $lunghezza = $i;
+                    if (isset($_POST['bottoni5'])) {
+                        if ($i == 0) {
+                            echo "<center>NO PREPRINTS!</center>";
+                        } else {
+                            #richiamo funzione per inserire i pdf all'interno del database
+                            insert_pdf();
+                            echo "<center>HAVE BEEN INCLUEDED " . $i . " PREPRINTS INTO DATABASE!</center>";
+                            #aggiorno la pagina dopo 2 secondi
+                            page_redirect("./check_preprints.php");
+                        }
+                    }
+                    #eliminazione pdf...
+                    if (isset($_POST['bottoni6'])) {
+                        for ($j = 0; $j < $lunghezza; $j++) {
+                            $percorso = $base . $array[$j];
+                            $delete = $_POST[$j];
+                            if ($delete == "checked") {
+                                $z++;
+                                if (is_dir($directory)) {
+                                    if ($directory_handle = opendir($directory)) {
+                                        while (($file = readdir($directory_handle)) !== false) {
+                                            if ((!is_dir($file)) & ($file != ".") & ($file != "..")) {
+                                                if ($file == $array[$j]) {
+                                                    #cancello file...
+                                                    unlink($percorso);
+                                                    #cancello riga database...
+                                                    remove_preprints($array[$j]);
+                                                }
+                                            }
                                         }
+                                        #Chiudo la lettura della directory.
+                                        closedir($directory_handle);
                                     }
                                 }
-                                #Chiudo la lettura della directory.
-                                closedir($directory_handle);
                             }
                         }
+                        if ($z == 0) {
+                            echo "<center>NOTHING SELECTED!</center><br/><br/><br/><br/>";
+                        } else {
+                            echo "<center>" . $z . " SELECTED PREPRINTS REMOVED! PAGE WILL BEEN UPDATED BETWEEN 2 SECONDS!</center><br/><br/><br/><br/>";
+                            #aggiorno la pagina dopo 2 secondi
+                            echo '<META HTTP-EQUIV="Refresh" Content="2; URL=./check_preprints.php">';
+                        }
                     }
-                }
-                if ($z == 0) {
-                    echo "<center>NOTHING SELECTED!</center><br/><br/><br/><br/>";
+                    if ($i == 0) {
+                        echo "<center>NO PREPRINTS!</center>";
+                    }
                 } else {
-                    echo "<center>" . $z . " SELECTED PREPRINTS REMOVED! PAGE WILL BEEN UPDATED BETWEEN 2 SECONDS!</center><br/><br/><br/><br/>";
-                    #aggiorno la pagina dopo 2 secondi
-                    page_redirect("./check_preprints.php");
+                    echo "<center><br/>ACCESS DENIED!</center>";
+                    echo '<META HTTP-EQUIV="Refresh" Content="2; URL=./reserved.php">';
                 }
-            }
-            if ($i == 0) {
-                echo "<center>NO PREPRINTS!</center>";
-            }
-            #funzione di aggiornamento della pagina...
-
-            function page_redirect($location) {
-                echo '<META HTTP-EQUIV="Refresh" Content="2; URL=' . $location . '">';
-                exit;
+            } else {
+                echo '<META HTTP-EQUIV="Refresh" Content="0; URL=./reserved.php">';
             }
             ?>
         </div>
